12146c21c5bd478c2967f86b10435607feba585c
leeduan <lee.duan1@gmail.com>
2014-01-04 16:04:39 -0800

    add timestamps to migrations for existing db tables

--- db/migrate/20140103043753_create_posts.rb
+++ db/migrate/20140103043753_create_posts.rb
@@ -7,3 +7,13 @@ class CreatePosts < ActiveRecord::Migration
     end
   end
 end
+
+### Alternative syntax for change method
+# def up
+#   create_table :posts do |t|
+#   end
+# end
+# roll back table, opposite up method
+# def down
+#   drop_table :posts
+# end
