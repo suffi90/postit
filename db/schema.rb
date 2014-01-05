6a34ccd75edd34ba841fdc340accf593b4a36cea
leeduan <lee.duan1@gmail.com>
2014-01-04 20:57:05 -0800

    add timestamps to post categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140105043218) do
+ActiveRecord::Schema.define(version: 20140105043852) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20140105043218) do
   end
 
   create_table "post_categories", force: true do |t|
-    t.integer "post_id"
-    t.integer "category_id"
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
