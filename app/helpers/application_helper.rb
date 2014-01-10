4bcd02c355897d0c31ed3e930e8f98c7ab78f04e
leeduan <lee.duan1@gmail.com>
2014-01-09 21:08:32 -0800

    add fix url and date beautify helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(url)
+    url.starts_with?('http://') ? url : "http://#{url}"
+  end
+
+  def print_date(date)
+    Time.at(date.to_i).strftime("%I:%M%p on %B %d, %Y")
+  end
 end
