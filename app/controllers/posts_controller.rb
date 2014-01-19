8e5f925d92d5f2babb571cfddb31acad58aeb6f8
leeduan <lee.duan1@gmail.com>
2014-01-18 18:53:38 -0800

    add votes to post#index route/view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by{ |post| post.total_votes }.reverse
   end
 
   def show
@@ -35,6 +35,16 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def vote
+    @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    if @vote.valid?
+      flash[:notice] = 'Your vote was counted.'
+    else
+      flash[:error] = 'You can only vote once.'
+    end
+    redirect_to :back
   end
 
   private
