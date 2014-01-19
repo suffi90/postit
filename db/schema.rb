db69230a96df2e14daa59795ccd3069cc90c2563
leeduan <lee.duan1@gmail.com>
2014-01-18 16:24:02 -0800

    add votes db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117035844) do
+ActiveRecord::Schema.define(version: 20140119002057) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 20140117035844) do
     t.string   "password_digest"
     t.string   "phone"
     t.string   "time_zone"
+  end
+
+  create_table "votes", force: true do |t|
+    t.boolean  "vote"
+    t.integer  "user_id"
+    t.string   "voteable_type"
+    t.integer  "voteable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
