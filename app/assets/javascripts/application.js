098798e01b83919e73ca663943c9ec93060b7221
leeduan <lee.duan1@gmail.com>
2014-01-23 22:07:51 -0800

    make text change to bootstrap popover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
   var options = {
     trigger: 'hover',
     title: 'Why do you need my phone number?',
-    content: 'We use your phone number for 2 factor authentication. 2-factor authentication provides an extra layer of security when logging in to secure systems. If you provide a phone number here, you will receive a text message with a pin number to access your account. To skip 2 factor authentication and login with just a username and password, leave this field blank. US numbers only.'
+    content: 'We use your phone number for Two-factor authentication. Two-factor authentication provides an extra layer of security when logging in to secure systems. If you provide a phone number here, you will receive a text message with a pin number to access your account. To skip two-factor authentication and login with just a username and password, leave this field blank. US numbers only.'
   }
 
   $('#two-factor-phone').popover(options);
